37ce71ef8fed714265c4f8c4d7321cd87685994d
mcarbonell <115166433+MateuCarbonell@users.noreply.github.com>
2025-01-07 17:28:28 +0100

    Update intranet.php

--- intranet.php
+++ intranet.php
@@ -11,15 +11,30 @@ if (!isset($_SESSION['username'])) {
 <html lang="es">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Intranet</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
-<body>
-    <h1>Bienvenido, <?php echo htmlspecialchars($_SESSION['username']); ?>!</h1>
-    <a href="logout.php">Cerrar sesión</a>
-    <br>
-    <a href="disable.php">Darse de baja</a>
-    <form action="actividad18.php">
-        
-    </form>
+<body class="bg-light">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-6 mt-5">
+                <div class="card">
+                    <div class="card-header text-center">
+                        <h3>Bienvenido, <?php echo htmlspecialchars($_SESSION['username']); ?>!</h3>
+                    </div>
+                    <div class="card-body">
+                        <a href="logout.php" class="btn btn-danger btn-block mb-3">Cerrar sesión</a>
+                        <a href="disable.php" class="btn btn-warning btn-block mb-3">Darse de baja</a>
+                        <img class="btn-block mb-3" src="noFilter.png" alt="gatoMewing">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
